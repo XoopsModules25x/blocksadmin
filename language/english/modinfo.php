6bcb5edad69eecdfb1df66cca4380645fa8126a2
mambax7 <mambax7@gmail.com>
2019-01-19 18:23:09 -0500

    help, language

--- language/english/modinfo.php
+++ language/english/modinfo.php
@@ -1,3 +1,19 @@
 <?php
 
+// The name of this module
+define('_MI_BLOCKSADMIN_NAME', 'Blocks Admin');
+
+//Help
+define('_MI_BLOCKSADMIN_DIRNAME', basename(dirname(dirname(__DIR__))));
+define('_MI_BLOCKSADMIN_HELP_HEADER', __DIR__ . '/help/helpheader.tpl');
+define('_MI_BLOCKSADMIN_BACK_2_ADMIN', 'Back to Administration of ');
+define('_MI_BLOCKSADMIN_OVERVIEW', 'Overview');
+
+//define('_MI_BLOCKSADMIN_HELP_DIR', __DIR__);
+
+//help multi-page
+define('_MI_BLOCKSADMIN_DISCLAIMER', 'Disclaimer');
+define('_MI_BLOCKSADMIN_LICENSE', 'License');
+define('_MI_BLOCKSADMIN_SUPPORT', 'Support');
+
 define('_BLOCKS_ADMIN_CUSTOM', 'Custom Blocks');
